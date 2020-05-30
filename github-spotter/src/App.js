b52851967397ea16c5d38577a1b4c012713bc22d
Vivesh Yadav <viveshofficial@gmail.com>
2020-05-30 14:34:01 +0545

    Get users info from github api

--- github-spotter/src/App.js
+++ github-spotter/src/App.js
@@ -1,7 +1,7 @@
 /*_____Imports from React Library________*/
 import React, { Component, Fragment } from 'react';
 import './App.css';
-
+import axios from 'axios';
 /*______Import Custom Component_________*/
 import Navbar from './components/layout/Navbar';
 import User from './components/users/User';
@@ -11,14 +11,25 @@ import User from './components/users/User';
 
 /*_______Component : App_________________*/
 class App extends Component {
+  state = {
+    users : [],
+    loading : false
+  }
+  async componentDidMount(){
+    this.setState({loading: true});
+    
+    const response = await axios.get('https://api.github.com/users');
 
+    this.setState({loading : false, users : response.data})
+
+  }
   render(){
   
     return (
       <Fragment>
          <Navbar title = "GitHub Spotter"  icon = "fab fa-github" />
         <div className="container">
-           <User />
+           <User loading={this.state.loading} users = {this.state.users}/>
         </div>
       </Fragment>
     );
--- github-spotter/src/components/users/User.js
+++ github-spotter/src/components/users/User.js
@@ -4,33 +4,11 @@ import React, { Component } from 'react'
 import UserItem from './UserItem';
 
 class User extends Component {
-    state = {
-        users:[
-            {
-                id : 1,
-                login : 'viveshCodes',
-                avatar_url :  "https://avatars0.githubusercontent.com/u/1?v=4",
-                html_url : "https://github.com/mojombo"
-            },
-            {
-                id : 2,
-                login : 'viveshCodes1',
-                avatar_url :  "https://avatars0.githubusercontent.com/u/1?v=4",
-                html_url : "https://github.com/mojombo"
-            },
-            {
-                id : 3,
-                login : 'viveshCodes2',
-                avatar_url :  "https://avatars0.githubusercontent.com/u/1?v=4",
-                html_url : "https://github.com/mojombo"
-            }
-            
-        ]
-    }
+    
     render() {
         return (
             <div style={userStyle}>
-                {this.state.users.map(user =>(
+                {this.props.users.map(user =>(
                     <UserItem key={user.id} user={user} />
                 ))}
                 
