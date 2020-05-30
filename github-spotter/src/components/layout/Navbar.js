09967827de5e6642454e92bec939921514ab3197
Vivesh Yadav <viveshofficial@gmail.com>
2020-05-30 11:41:01 +0545

    Add functional component for navbar and useritem

--- github-spotter/src/components/layout/Navbar.js
+++ github-spotter/src/components/layout/Navbar.js
@@ -1,25 +1,26 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
-class Navbar extends Component {
-    static defaultProps = {
-        title : 'GitHub Spotter',
-        icon : 'fab fa-github'
-    };
-    static propTypes = {
-        title : PropTypes.string.isRequired,
-        icon : PropTypes.string.isRequired
-    }
-    render() {
+const Navbar = (props) => {
+    const {icon, title} = props;
         return (
             <nav className = "navbar bg-primary">
                 <h1>
-                    <i className = {this.props.icon} />
-                    {this.props.title}
+                    <i className = {icon} />
+                    {title}
                 </h1>
             </nav>
         )
-    }
+   
+}
+
+Navbar.defaultProps = {
+    title : 'GitHub Spotter',
+    icon : 'fab fa-github'
+};
+Navbar.propTypes = {
+    title : PropTypes.string.isRequired,
+    icon : PropTypes.string.isRequired
 }
 
 export default Navbar;
--- github-spotter/src/components/users/Useritem.js
+++ github-spotter/src/components/users/Useritem.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 
-class UserItem extends Component {
-    
-    render() {
-        const { login, avatar_url, html_url } = this.props.user;
+const UserItem = (props) => {
+
+        const { login, avatar_url, html_url } = props.user;
         return (
             <div className = "card text-center">
                 <img 
@@ -18,8 +18,11 @@ class UserItem extends Component {
                     <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
                 </div>
             </div>
-        )
-    }
+        )  
+}
+
+UserItem.propTypes = {
+    user : PropTypes.object.isRequired,
 }
 
 export default UserItem
